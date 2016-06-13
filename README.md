b13114e1b4d6f6574b1b659a2ee825172a78949c
Tomas Tomecek <ttomecek@redhat.com>
2016-06-13 15:16:01 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# download-manifest-from-dockerhub
-Simple pythin script to download manifest from Docker Hub.
+# download-manifest.py
+
+```
+$ ./download-manifest.py registry
+{
+  "schemaVersion": 1,
+  "fsLayers": [
+    {
+      "blobSum": "sha256:a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422d00e8a7c22955b46d4"
+    },
+    {
+      "blobSum": "sha256:a3ed95caeb02ffe68cdd9fd84406680ae93d633cb16422d00e8a7c22955b46d4"
+    },
+...
+```
+
+For more information check [this blog post](http://blog.tomecek.net/post/download-manifests-from-docker-hub/)
+.
---
+++ download-manifest.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import sys
+import json
+
+import requests
+
+
+login_template = "https://auth.docker.io/token?service=registry.docker.io&scope=repository:{repository}:pull"
+get_manifest_template = "https://registry.hub.docker.com/v2/{repository}/manifests/{tag}"
+
+
+def pretty_print(d):
+    print(json.dumps(d, indent=2))
+
+
+def download_manifest_for_repo(repo, tag):
+    """
+    repo: string, repository (e.g. 'library/fedora')
+    tag:  string, tag of the repository (e.g. 'latest')
+    """
+    response = requests.get(login_template.format(repository=repo), json=True)
+    response_json = response.json()
+    token = response_json["token"]
+    response = requests.get(
+        get_manifest_template.format(repository=repo, tag=tag),
+        headers={"Authorization": "Bearer {}".format(token)},
+        json=True
+    )
+    manifest = response.json()
+    if not response.status_code == requests.codes.ok:
+        pretty_print(dict(response.headers))
+    return manifest
+
+
+def main():
+    repos = sys.argv[1:]
+    if not repos:
+        print("Usage: {} <[namespace/]repository[:tag]> [<[namespace/]repository[:tag]>...]".format(sys.argv[0]) +
+              "\nExample: {} fedora:23".format(sys.argv[0]))
+        return 1
+    for repo_tag in repos:
+        if ":" in repo_tag:
+            repo, tag = repo_tag.split(":")
+        else:
+            repo, tag = repo_tag, "latest"
+        if "/" not in repo:
+            repo = "library/" + repo
+        pretty_print(download_manifest_for_repo(repo, tag))
+        return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
+
